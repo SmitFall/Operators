005a8b8aaac936a35b074f6742530d4b0cb6fe72
Smitfall <captainfalconrsmith@gmail.com>
2022-01-12 08:44:20 -0700

    Add project files.

---
+++ Operators/Operators.vb
@@ -0,0 +1,32 @@
+﻿Module Operators
+
+    Sub Main()
+        'example of '+' operator
+        Console.Write("5 + 5 = ")
+        Console.WriteLine(5 + 5)
+        Console.WriteLine() 'add a blank line for reading
+        'example'*' operator
+        Console.Write("5 * 5 = ")
+        Console.WriteLine(5 * 5)
+        Console.WriteLine()
+        'example '^' operator
+        Console.Write("5 ^ 2 = ")
+        Console.WriteLine(5 ^ 2)
+        Console.WriteLine()
+        'example of '/' operator. floating poit division
+        Console.Write("5 / 2 = ")
+        Console.WriteLine(5 / 2)
+        Console.WriteLine()
+        'example of '\' operator interger division
+        Console.Write("5 \ 2 = ")
+        Console.WriteLine(5 \ 2)
+        Console.WriteLine()
+        'example of 'Mod' operator.
+        'remander from operator division
+        Console.Write("5 Mod 2 = ")
+        Console.WriteLine(5 Mod 2)
+        Console.WriteLine()
+        Console.Read() 'wait for user to respone 
+    End Sub
+
+End Module
